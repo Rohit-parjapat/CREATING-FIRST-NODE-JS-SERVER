136c849bdc6b325a92a959f0edf1271212ce6cb2
Rohit-parjapat <rparjapati7377@gmail.com>
2022-08-21 10:18:53 +0530

    update after install nodemon

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = 8081;
 http
     .createServer((request, response) => {
         response.writeHead(200, { 'content-type': "text/html" });
-        response.write("<h1>Hello, This is from my server</h1>");
+        response.write("<h1>Hello, This is from my first server</h1>");
         response.end();
     })
     .listen(port, () => {
-        console.log('Nodejs server started on ${port}');
+        console.log(`Nodejs server started on ${port}`);
     })
 
 //http://localhost:8081
